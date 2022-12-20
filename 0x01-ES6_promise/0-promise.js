f6813b074f976c0777c4c3e720a8be50ce796496
GMercury <99487197+GMercury@users.noreply.github.com>
2022-12-20 18:19:03 +0300

    Keep every promise you make and only make promises you can keep

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,7 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+  // Perform the asynchronous operation here
+  // If the operation is successful, call resolve with the result
+  // If the operation fails or encounters an error, call reject with the error
+    });
+}

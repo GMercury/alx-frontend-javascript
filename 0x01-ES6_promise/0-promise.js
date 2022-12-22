5f649312ce848531acf631904ffbf052e166e7ee
GMercury <99487197+GMercury@users.noreply.github.com>
2022-12-22 07:42:57 +0300

    0x01-ES6_promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,3 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-  // Perform the asynchronous operation here
-  // If the operation is successful, call resolve with the result
-  // If the operation fails or encounters an error, call reject with the error
-    });
+  return new Promise(() => {});
 }

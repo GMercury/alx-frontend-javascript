57f716d9210dd900ac422c40d3b881660d6a8607
GMercury <99487197+GMercury@users.noreply.github.com>
2023-01-03 20:52:06 +0300

    Update 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,3 +1,4 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   return students.filter((std) => std.location === city).map((std) => ({ ...std, grade: newGrades.filter((g) => g.studentId === std.id).map((g) => g.grade)[0] || 'N/A' }));
 }
+
